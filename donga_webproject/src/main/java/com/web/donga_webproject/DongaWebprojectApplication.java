16a5ef24deaf5ae628153bd6e922e24d36bf2250
kimdonghyeon <lghan00020@nate.com>
2023-10-04 22:49:42 +0900

    spring boot 개발 환경 세팅

---
+++ donga_webproject/src/main/java/com/web/donga_webproject/DongaWebprojectApplication.java
@@ -0,0 +1,13 @@
+package com.web.donga_webproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongaWebprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongaWebprojectApplication.class, args);
+	}
+
+}
---
+++ donga_webproject/src/test/java/com/web/donga_webproject/DongaWebprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.donga_webproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongaWebprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
